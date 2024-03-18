c46a4e46fa867fadc3b67c88798686d3bdf61907
dKenez <47757462+dKenez@users.noreply.github.com>
2024-03-18 03:00:52 +0100

    Modifies file upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
 	import { FileDropzone } from '@skeletonlabs/skeleton';
-	import { UploadSolid } from 'flowbite-svelte-icons';
+	import { UploadSolid, FileImageSolid } from 'flowbite-svelte-icons';
+
+	let files: FileList;
+	$: noFileSelected = !files || files.length === 0;
+	$: {
+		if (!noFileSelected) {
+			const file = files[0];
+			if (file && file.type.startsWith('image/')) {
+				const reader = new FileReader();
+				reader.onload = () => {
+					const imageUrl = reader.result;
+					let imgDropzone = document.getElementById('img-dropzone');
+					if (imgDropzone) imgDropzone.style.backgroundImage = `url(${imageUrl})`;
+				};
+				reader.readAsDataURL(file);
+			}
+		}
+	}
+
+	let backgroundImage: string;
+
+	function onChangeHandler(e: Event): void {
+		// console.log('file data:', e);
+	}
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
@@ -16,16 +39,42 @@
 		</p>
 		<p>To get started, upload a file into the dropzone below:</p>
 
-		<FileDropzone name="files">
-			<svelte:fragment slot="lead">
-				<UploadSolid class="w-[48px] h-[48px] text-gray-800 dark:text-white block mx-auto" />
-			</svelte:fragment>
-			<svelte:fragment slot="message"><b>Upload a file</b> or drag and drop</svelte:fragment>
-			<svelte:fragment slot="meta"
-				><small>
-					Please upload only <code class="code">.png</code> files - support for more files coming soon(ish)!</small
-				></svelte:fragment
+		<div id="img-dropzone" class="bg-cover bg-center rounded-lg">
+			<FileDropzone
+				name="files"
+				on:change={onChangeHandler}
+				bind:files
+				id="test"
+				padding={noFileSelected ? 'p-24' : 'p-24 !bg-white/[0] backdrop-blur-sm'}
+				regionInterface={noFileSelected ? 'p-8' : 'p-8 bg-black/[0.5] backdrop-blur-xl rounded-md'}
 			>
-		</FileDropzone>
+				<svelte:fragment slot="lead">
+					{#if noFileSelected}
+						<UploadSolid class="w-[48px] h-[48px] text-gray-800 dark:text-white block mx-auto" />
+					{:else}
+						<FileImageSolid class="w-[48px] h-[48px] text-gray-800 dark:text-white block mx-auto" />
+					{/if}
+				</svelte:fragment>
+				<svelte:fragment slot="message">
+					{#if noFileSelected}
+						<b>Upload a file</b> or drag and drop
+					{:else}
+						Predicting location of <code class="code">{files[0].name}</code>
+					{/if}
+				</svelte:fragment>
+				<svelte:fragment slot="meta">
+					<small>
+						Please only upload <code class="code">.jpg</code> files - support for more files coming soon(ish)!</small
+					>
+				</svelte:fragment>
+			</FileDropzone>
+		</div>
+		<div class="h-[200px] w-[200px]" id="test" />
 	</div>
 </div>
+
+<style>
+	#test {
+		width: 100%;
+	}
+</style>
