bdfd49a9ca01159703530b4a4d0ebdbebd8b238d
KarolRafalCharylo <charylokarol@gmail.com>
2024-07-18 18:15:56 +0200

    Color changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end" class="sticky top-0 z-[50000] backdrop-blur !bg-surface-800/80 border-b border-surface-500/30">
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end" class="sticky top-0 z-[50000] backdrop-blur !bg-surface-800/80 border-b border-surface-100/30">
 	<svelte:fragment slot="lead"><Logo /></svelte:fragment>
 	<h1 class="h1">MoldAI</h1>
 	<svelte:fragment slot="trail"><AppBarActions /></svelte:fragment>
